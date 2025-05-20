e1bde67812fd3d0c1e93c5ac1b8f259dd5f9605e
MatheusErcoli <matheusercoli@gmail.com>
2025-05-19 21:52:57 -0300

    usei php para fazer o template(foi dificil) e comecei a fazer a página home

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MCE Celulares</title>
+    <base href="http://localhost/MCE-Celulares/">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
@@ -11,14 +12,14 @@
     <header>
     <nav class="navbar navbar-expand-lg bg-purple fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand white" href="index.php"><img src="logo.png" alt="logo" width="100" height="auto"></a>
+    <a class="navbar-brand white" href="home"><img src="logo.png" alt="logo" width="100" height="auto"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item linha">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" aria-current="page" href="home">Home</a>
         </li>
         <li class="nav-item linha">
           <a class="nav-link" href="">Serviços</a>
@@ -57,7 +58,6 @@
         include("paginas/404.php");
     }
     ?>
-
 </main>
 
 
---
+++ paginas/home.php
@@ -0,0 +1,11 @@
+<div class="container px-4 text-center">
+    <h1 class="titulo">O que você <strong>Precisa?</strong></h1>
+  <div class="row gx-5">
+    <div class="col-12 col-md-6">
+      <div class="p-3 quadrado"><p>precisa de assistência técnica?</p><a href="servicos" class="btn btn-purple">Clique Aqui</a></div>
+    </div>
+    <div class="col-12 col-md-6">
+      <div class="p-3 quadrado"><p>Quer ver nossos produtos?</p><a href="produtos" class="btn btn-purple">Clique Aqui</a></div>
+    </div>
+  </div>
+</div>
