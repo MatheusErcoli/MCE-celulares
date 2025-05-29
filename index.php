a5a7a5067e55082d5957bb996e102874fc09c39c
MatheusErcoli <matheusercoli@gmail.com>
2025-05-29 19:53:39 -0300

    começo da página produtos

--- index.php
+++ index.php
@@ -37,12 +37,12 @@
               <a class="nav-link hover" href="servicos">Serviços</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle hover" href="#" role="button" data-bs-toggle="dropdown"
+              <a class="nav-link dropdown-toggle hover" href="produtos" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 Produtos
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Celulares</a></li>
+                <li><a class="dropdown-item" href="produtos">Celulares</a></li>
                 <li><a class="dropdown-item" href="#">X</a></li>
                 <li><a class="dropdown-item" href="#">Y</a></li>
               </ul>
---
+++ paginas/produtos.php
@@ -0,0 +1,19 @@
+<main class="conteudo container">
+<aside>
+    <ul>
+        <h5 class="text-center">Atalho:</h5>
+        <li><a href="#iphone">Apple iphone</a></li>
+        <li><a href="#samsung">Samsung</a></li>
+        <li><a href="#Galaxy">Samsung Galaxy A</a></li>
+        <li><a href="#Xiaomi">Xiaomi</a></li>
+        <li><a href="#Redmi">Redmi</a></li>
+        <li><a href="#Poco">Poco</a></li>
+        <li><a href="#videogames">Videogames e jogos</a></li>
+        <li><a href="#notebook">Notebooks e Periféricos</a></li>
+    </ul>
+    </aside>  
+<section class="margin-top text-center">
+    <h1>Produtos:</h1>
+</section>
+
+</main>
