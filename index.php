404273afecb5764c3a312fcff2e1a3234f377a45
MatheusErcoli <matheusercoli@gmail.com>
2025-06-11 14:19:04 -0300

    aqui acabou

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$base = "https://{$_SERVER['SERVER_NAME']}{$_SERVER['SCRIPT_NAME']}"
+$base ="https://" . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,7 +8,8 @@ $base = "https://{$_SERVER['SERVER_NAME']}{$_SERVER['SCRIPT_NAME']}"
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MCE Celulares</title>
-  <base href="<?= $base ?>">
+  <base href="<?=$base?>"> 
+   <link rel="shortcut icon" href="/imagens/celu.png" type="image/x-icon" height="50">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   <link rel="stylesheet" href="css/estilo.css">
