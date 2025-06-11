9006741129ef42ccd17ba5ce6b6bb0e2a88fe843
MatheusErcoli <matheusercoli@gmail.com>
2025-06-11 12:47:05 -0300

    mais um ajuste na base do site para hostear

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+$base = "https://{$_SERVER['SERVER_NAME']}{$_SERVER['SCRIPT_NAME']}"
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -5,7 +8,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MCE Celulares</title>
-  <base href="https://mce-celulares.infinityfreeapp.com">
+  <base href="<?= $base ?>">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   <link rel="stylesheet" href="css/estilo.css">
